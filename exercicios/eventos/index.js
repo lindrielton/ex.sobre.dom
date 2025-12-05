777aef2580c5983f814838503710eaf88a7424ea
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-12-04 21:32:16 -0300

    eventos

---
+++ exercicios/eventos/index.js
@@ -0,0 +1,23 @@
+function onmause(){
+    console.log('onmauseenter')
+}
+
+
+function onchang(){
+    console.log('digitou e mudou o valor')
+}
+
+
+function focu(){
+     console.log('entrou e clicou no elemento')
+     //só acontece au clicar
+}
+
+
+function saida(){
+    console.log('saindo de um elemento')
+}
+
+function clicou(){
+    console.log('clicou')
+}
